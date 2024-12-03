7ed0ecc627027ba0b98b26e035a35de7a402f507
Chang M. Yun <cmyun@sh04-ln07.stanford.edu>
2024-12-02 20:44:44 -0800

    First working version. With Protein encoder: ESM2(650M), DNA encoder: BPNet(ab initio).

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='ImmunoAlign',
+    name='protein-dna-clip',
     version='0.0.1',
     packages=find_packages(),
-    install_requires=[
-        'torch',
+    install_requires=[        
+        "numpy >= 1.14.2",
+        "scipy >= 1.0.0",
+        "pandas >= 1.3.3",
+        "torch >= 1.9.0",
         'pytorch-lightning',
-        # add other dependencies here
+        "pytables >= 3.6.1",
+        "h5py >= 3.7.0",
+        "tqdm >= 4.64.1",
+        "seaborn >= 0.11.2",
+        "biopython >= 1.79",
+        "modisco-lite >= 2.0.0",
+        "tangermeme >= 0.2.3"
     ],
 )
